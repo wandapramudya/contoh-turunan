29e73d974e341b18e780755d8b46bb6492caa7bd
wandapramudya <wandapramudya9@gmail.com>
2025-05-14 19:52:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello Streamlit-er 👋")
+st.title("Hello Streamlit-er 👋 Wanda")
 st.markdown(
     """ 
     This is a playground for you to try Streamlit and have fun. 
